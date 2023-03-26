aa14a486c47c4575ab5d993f656ac789292778e6
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-03-26 01:24:49 -0700

    upate

--- diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
+++ diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
@@ -63,7 +63,7 @@ class KitchenMjlLowdimDataset(BaseLowdimDataset):
                 episode = {
                     'obs': obs,
                     'action': data['ctrl'].astype(np.float32),
-                    'lang': np.array([[1, 2, 3, 4, 5]]) #np.cumsum(np.repeat(np.array([[1, 2, 3, 4, 5]]), obs.shape[0], axis=0), axis=0) #lang
+                    'lang': np.cumsum(np.repeat(np.array([[1, 2, 3, 4, 5]]), obs.shape[0], axis=0), axis=0) #lang
                 }
                 #import pdb;pdb.set_trace()
                 self.replay_buffer.add_episode(episode)
