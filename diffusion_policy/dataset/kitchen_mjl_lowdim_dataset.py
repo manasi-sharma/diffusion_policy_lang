70e59345891a6eccd405cd0a9628356a53de03db
Manasi Sharma <45543113+manasi-sharma@users.noreply.github.com>
2023-03-29 14:11:20 -0700

    upate

--- diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
+++ diffusion_policy/dataset/kitchen_mjl_lowdim_dataset.py
@@ -65,12 +65,12 @@ class KitchenMjlLowdimDataset(BaseLowdimDataset):
                     'action': data['ctrl'].astype(np.float32),
                     'lang': np.cumsum(np.repeat(np.array([[1, 2, 3, 4, 5]]), obs.shape[0], axis=0), axis=0) #lang
                 }
-                #import pdb;pdb.set_trace()
+                import pdb;pdb.set_trace()
                 self.replay_buffer.add_episode(episode)
             except Exception as e:
                 print(i, e)
 
-        import pdb;pdb.set_trace()
+        #import pdb;pdb.set_trace()
         val_mask = get_val_mask(
             n_episodes=self.replay_buffer.n_episodes, 
             val_ratio=val_ratio,
